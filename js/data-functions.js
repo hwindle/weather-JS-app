fb8f8d78a8e8cd68096f2ef52668c60084389a8e
Hazel Windle <hwindle83@googlemail.com>
2023-01-21 13:05:13 +0000

    Planned out JavaScript functions

---
+++ js/data-functions.js
@@ -0,0 +1,9 @@
+/*** get location, from coords if given,  ***/
+
+// getWeather(location, full-weather)
+
+// processWeather(parsed Json, full-weather)
+
+// setWeatherLocation(location)
+
+// getWeatherLocation(location)
---
+++ js/index.js
@@ -0,0 +1,16 @@
+import { getLocation, getWeather } from './data-functions';
+
+/*** DOM functions ***/
+// createFigure
+
+// createTable
+
+// createOneDaySection
+
+// addToDOM (adds weather-one-day section to area (still a template))
+
+// updateWeather(id, full) puts weather info in a card with html id
+
+/*** Getting Geolocation data if device has this ***/
+
+// Run these items when the app first starts
